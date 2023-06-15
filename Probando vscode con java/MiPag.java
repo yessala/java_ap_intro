28487324b6c03228a8a3ef3803f207aae479b23a
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-06-15 18:16:06 -0300

    agregado archivo de ejemplo

---
+++ Probando vscode con java/MiPag.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MiPag{
+
+    public static void main (String[] args){
+
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Dime hola");
+        String saludo = leer.nextLine();
+
+        System.out.println(saludo);
+
+    }
+
+}
