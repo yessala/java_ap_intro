7dfa9765dd3a6afa783756883660e00a017a9c22
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-06-15 13:07:36 -0300

    arreglando codigo de vscode, listo

--- Probando vscode con java/MiPrimeraClaseEnVSCode.java
+++ Probando vscode con java/MiPrimeraClaseEnVSCode.java
@@ -1,10 +1,7 @@
 import java.util.Scanner;
 
 public class MiPrimeraClaseEnVSCode {
-    
-    /**
-     * @param leer
-     */
+ 
     public static void main(String[] args) {
         
         Scanner leer = new Scanner(System.in);
