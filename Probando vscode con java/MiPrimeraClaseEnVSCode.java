19e7e2cc368ec3d081a43760bb43da1b8c62f798
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-06-15 12:38:58 -0300

    primer commit de Argentina Programa con gitignore

---
+++ Probando vscode con java/MiPrimeraClaseEnVSCode.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MiPrimeraClaseEnVSCode {
+    
+    /**
+     * @param leer
+     */
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero para verificar si es par ó impar");
+        int num = leer.nextInt();
+
+        System.out.println(parImpar(num));
+
+    }
+
+    public static String parImpar(int num){
+
+
+       String resultado = (num%2<=0) ? "El número es par" : "El número es impar";
+
+       return resultado;
+    }
+
+}
