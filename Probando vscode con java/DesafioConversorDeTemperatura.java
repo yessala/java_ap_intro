0c0946af73bd4029a5a69f9073d304fd22c6a31b
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-06-16 18:00:51 -0300

    arreglar el desafio de temperatura

--- Probando vscode con java/DesafioConversorDeTemperatura.java
+++ Probando vscode con java/DesafioConversorDeTemperatura.java
@@ -1,23 +1,39 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
+
 public class DesafioConversorDeTemperatura {
 
     static Scanner leer = new Scanner(System.in);
+
     public static void main(String[] args) {
-      
-        try{
-            System.out.println("Ingrese temperatura en grados Celsius a convertir");
-            double celsius=leer.nextDouble();
 
-            double fahrenheit = (celsius * 9/5) + 32;
+        System.out.println("Ingrese temperatura en grados Celsius a convertir");
+        double grado = leer.nextDouble();
+        leer.nextLine();
+        System.out.println("Seleccione el tipo de temperatura que ingresó C(celsius)/F(fahreneit)");
+        String tipo = leer.nextLine();
+        if (tipo.equalsIgnoreCase("C")) {
+            try {
 
-            System.out.println(celsius + "° C equivale a " + fahrenheit + "° F.");
+                double fahrenheit = (grado * 9 / 5) + 32;
 
-        } catch(InputMismatchException e){
-            System.out.println("Error: formato de temperatura equivocado");
+                System.out.println(grado + "° C equivale a " + fahrenheit + "° F.");
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: formato de temperatura equivocado");
+            }
+        } else if (tipo.equalsIgnoreCase("F")) {
+            try {
+
+                double celsius = (grado - 32) * 5 / 9;
+
+                System.out.println(grado + "° F equivale a " + celsius + "° C.");
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: formato de temperatura equivocado");
+            }
+
         }
-
     }
 
-   
 }
---
+++ Probando vscode con java/DesafioValidarContrasenia.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class DesafioValidarContrasenia {
+    
+    static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+
+}
