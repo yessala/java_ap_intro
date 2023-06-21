18239690e643a160cb7fd51686a2b26cc1b40fe4
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-06-21 17:33:00 -0300

    ejercicios de la guia dia 3

--- Probando vscode con java/Dia2SwitchGeometria.java
+++ Probando vscode con java/Dia2SwitchGeometria.java
@@ -43,4 +43,5 @@ public class Dia2SwitchGeometria {
 
         }
     }
+   
 }
---
+++ Probando vscode con java/Dia3ArraySuma.java
@@ -0,0 +1,16 @@
+// Escribir un programa que cree un array de 3 enteros e imprima por consola la suma de todos sus elementos, sin usar el bucle for.
+import java.util.Arrays;
+
+public class Dia3ArraySuma {
+    public static void main(String[] args) {
+        //definir valores de la array
+        int[] value = {1,2,3,4,5,7};
+        //llamo la clase Arrays con su metodo stream para acceder a los valores, luego la funcion de java sum() que me suma los valores 
+        int suma = Arrays.stream(value).sum();
+
+        System.out.println("La suma de todos los objetos de la array es: " + suma);
+
+    }
+}
+//esta es otra forma de hacer la suma
+//Arrays.stream(value).parallel().reduce(0,(a,b)->  a + b)
