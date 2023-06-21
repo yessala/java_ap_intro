6dc47bbcc2fa1fee6f32805c9db80ddcfa993e27
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-06-21 13:37:23 -0300

    dia 2 validar contraseña predeterminada

---
+++ Probando vscode con java/Dia2Contrasenia.java
@@ -0,0 +1,19 @@
+// Escribir un programa que solicite al usuario una contraseña y verifique si coincide con una contraseña predefinida. Si la contraseña ingresada es correcta, mostrar un mensaje de "Acceso concedido". De lo contrario, mostrar un mensaje de "Acceso denegado".
+import java.util.Scanner;
+
+public class Dia2Contrasenia {
+    static Scanner leer = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Ingrese su Contraseña: ");
+        String password = leer.nextLine();
+        //Contraseña definida para comparar
+        String validPassword = "contra1234";
+        
+        //Validación de contraseña
+        if (password.equals(validPassword)){
+            System.out.print("Acceso Concedido");
+        }else
+        System.out.println("Acceso Denegado");
+    }
+}
