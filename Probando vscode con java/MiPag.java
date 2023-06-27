8765842b1e38951a030f648b063ff7ad68fcdae1
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-06-27 15:22:11 -0300

    Jugando con java en el vscode

--- Probando vscode con java/MiPag.java
+++ Probando vscode con java/MiPag.java
@@ -1,17 +1,68 @@
-import java.util.Scanner;
+import java.util.Arrays;
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        // Create two matrices
+        int[][] matrix1 = new int[][] {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int[][] matrix2 = new int[][] {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        // Multiply the matrices
+        int[][] product = Matrix.multiply(matrix1, matrix2);
+        // Print the product
+        System.out.println(Arrays.deepToString(product));
+    }
+}
 
-public class MiPag{
-    static Scanner leer = new Scanner(System.in);
 
-    public static void main (String[] args){
 
+
+// import java.util.Scanner;
+// import java.util.Arrays;
+// import java.util.Matrix;
+
+// public class MiPag{
+//     static Scanner leer = new Scanner(System.in);
+
+
+//     public static void main (String[] args){
+
+//        int[][] arr = {{1,2},{3,4}};
+//        int[][] arr2 = {{3,4},{1,2}};
+        
         
 
-        System.out.println("Dime hola");
-        String saludo = leer.nextLine();
+       //         int[][] arr3 = multiply(arr,arr2);
+        
+//         System.out.println(Arrays.deepToString(arr3));
+       
 
-        System.out.println(saludo);
+//     }
+//     public static int[][] multiply(int[][] a, int[][] b) {
+//     int[][] c = new int[a.length][b[0].length];
+//     // se comprueba si las matrices se pueden multiplicar
+//     if (a[0].length == b.length) {
+//         for (int i = 0; i < a.length; i++) {
+//             for (int j = 0; j < b[0].length; j++) {
+//                 for (int k = 0; k < a[0].length; k++) {
+//                     // aquí se multiplica la matriz
+//                     c[i][j] += a[i][k] * b[k][j];
+//                 }
+//             }
+//         }
+//     }
+//     /**
+//      * si no se cumple la condición se retorna una matriz vacía
+//      */
+//     return c;
+// }
+ 
 
-    }
 
-}
+// }
+// }
